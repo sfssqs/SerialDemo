f200c939151b60a6790d670fded5df122d08e07a
xiaxing <xiaxingwork@gmail.com>
2015-12-09 10:42:48 +0800

    initialize the serial demo project

---
+++ src/SerialDemo.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+#include<string.h>
+
+#include "serial.h"
+
+#define DEV_NAME "/dev/ttymxc1"
+
+int main(int argc, char *argv[]) {
+	int fd;
+	int len, ret;
+	char buf[] = "hello";
+
+	fd = open(DEV_NAME, O_RDWR | O_NOCTTY);
+	if (fd < 0) {
+		perror("Open device error\n");
+	}
+	printf("Device opened : %s\n", DEV_NAME);
+
+	/* 9600 8n1     150 255*/
+	ret = set_port_attr(fd, B9600, 8, "1", 'N', 0, 0);
+	if (ret < 0) {
+		printf("Set attribute failed \n");
+		exit(-1);
+	}
+	puts("Device attribute : (B9600 8n1)  (vtime 0)  (vmin 0)\n");
+
+	len = write(fd, buf, sizeof(buf));
+	if (len < 0) {
+		printf("Write data error!\n");
+		return -1;
+	}
+	printf("Output data : %s \n", buf);
+
+	memset(buf, 0, sizeof(buf));
+	len = read(fd, buf, sizeof(buf));
+	if (len < 0) {
+		printf("Read data error!\n");
+		return -1;
+	}
+	printf("Read length : %d \n", len);
+	printf("Read data : %s \n", buf);
+
+	close(fd);
+	printf("Device closed  : %s\n", DEV_NAME);
+
+	return (0);
+}
+
---
+++ src/serial.h
@@ -0,0 +1,99 @@
+#ifndef SERIAL_H_
+#define SERIAL_H_
+#endif
+
+static void set_baudrate(struct termios *opt, unsigned int baudrate) {
+	cfsetispeed(opt, baudrate);
+	cfsetospeed(opt, baudrate);
+}
+
+static void set_data_bit(struct termios *opt, unsigned int databit) {
+	opt->c_cflag &= ~CSIZE;
+	switch (databit) {
+	case 8:
+		opt->c_cflag |= CS8;
+		break;
+	case 7:
+		opt->c_cflag |= CS7;
+		break;
+	case 6:
+		opt->c_cflag |= CS6;
+		break;
+	case 5:
+		opt->c_cflag |= CS5;
+		break;
+	default:
+		opt->c_cflag |= CS8;
+		break;
+	}
+}
+
+static void set_parity(struct termios *opt, char parity) {
+	switch (parity) {
+	case 'N':
+		/* 无校验 */
+		/* 偶校验 */
+		/* 奇校验 */
+	case 'n':
+		opt->c_cflag &= ~PARENB;
+		break;
+	case 'E':
+	case 'e':
+		opt->c_cflag |= PARENB;
+		opt->c_cflag &= ~PARODD;
+		break;
+	case 'O':
+	case 'o':
+		opt->c_cflag |= PARENB;
+		opt->c_cflag |= ~PARODD;
+		break;
+		/* 其它选择为无校验 */
+	default:
+		opt->c_cflag &= ~PARENB;
+		break;
+	}
+}
+
+static void set_stopbit(struct termios *opt, const char *stopbit) {
+	if (0 == strcmp(stopbit, "1")) {
+		opt->c_cflag &= ~CSTOPB;
+		/* 1 位停止位 t */
+		/* 1.5 位停止位 */
+		/* 2 位停止位 */
+		/* 1 位停止位 */
+	} else if (0 == strcmp(stopbit, "1.5")) {
+		opt->c_cflag &= ~CSTOPB;
+	} else if (0 == strcmp(stopbit, "2")) {
+		opt->c_cflag |= CSTOPB;
+	} else {
+		opt->c_cflag &= ~CSTOPB;
+	}
+}
+
+static int set_port_attr(int fd, int baudrate, int databit, const char *stopbit,
+		char parity, int vtime, int vmin) {
+	struct termios opt;
+
+	tcgetattr(fd, &opt);
+
+	set_baudrate(&opt, baudrate);
+
+	// [Begin] :  write success but read failed
+	opt.c_cflag |= CLOCAL | CREAD;
+	// opt.c_cflag &= ~CRTSCTS;
+	//[End]
+
+	/* | CRTSCTS */
+	set_data_bit(&opt, databit);
+	set_parity(&opt, parity);
+	set_stopbit(&opt, stopbit);
+	opt.c_oflag = 0;
+	opt.c_lflag |= 0;
+	opt.c_oflag &= ~OPOST;
+	opt.c_cc[VTIME] = vtime;
+	opt.c_cc[VMIN] = vmin;
+	tcflush(fd, TCIFLUSH);
+
+	return (tcsetattr(fd, TCSANOW, &opt));
+}
+
